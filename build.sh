bf361dfe7d6aaa18d02698d271840497a3c43a2e
Shiro <ryukenzych@gmail.com>
2026-08-31 06:09:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,10 +28,15 @@ export LD_LIBRARY_PATH="$CLANG_DIR/lib:$LD_LIBRARY_PATH"
 
 if ! [ -d "${CLANG_DIR}" ]; then
 echo "Clang not found! Cloning to ${CLANG_DIR}..."
-if ! git clone --depth=1 https://github.com/kdrag0n/proton-clang.git ${CLANG_DIR}; then
-echo "Cloning failed! Aborting..."
+mkdir -p "${CLANG_DIR}"
+wget -q https://github.com/Shirotachii/google-clang-mirror/releases/download/clang-r547379-aosp/clang.tar.gz -O clang.tar.gz
+if [ $? -ne 0 ]; then
+echo "Download failed! Aborting..."
 exit 1
 fi
+echo "Extracting clang to ${CLANG_DIR}..."
+tar -xf clang.tar.gz -C "${CLANG_DIR}"
+rm -f clang.tar.gz
 fi
  
 if ! [ -d "${GCC_64_DIR}" ]; then
